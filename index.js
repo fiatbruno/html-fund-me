8783dd1d666ec0d0f23d01e7b5609507db714221
Fiat Bruno <fiatbruno10@gmail.com>
2023-01-03 07:15:03 +0200

    Added fund functionality

--- index.js
+++ index.js
@@ -31,8 +31,24 @@ async function fund() {
             const transactionResponse = await contract.fund({
                 value: ethers.utils.parseEther(ethAmount),
             })
+            await listenForTransactionMine(transactionResponse, provider)
+            console.log("Done!🤝")
         } catch (error) {
             console.log(error)
         }
+    } else {
+        fundBtn.innerHTML = "Plz install Metamask"
     }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+    console.log(`Mining ${transactionResponse.hash}...`)
+    return new Promise((resolve, reject) => {
+        provider.once(transactionResponse.hash, (transactionReceipt) => {
+            console.log(
+                `Completed with ${transactionReceipt.confirmations} confirmations`
+            )
+        })
+        resolve()
+    })
 }
